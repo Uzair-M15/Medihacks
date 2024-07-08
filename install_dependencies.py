6e475aeac944d988c55c6a6179906ace48c069ed
uzair <uzair@september11th.uzairhome.com>
2024-07-08 16:22:13 +0200

    [4]2024-07-08

--- install_dependencies.py
+++ install_dependencies.py
@@ -6,7 +6,7 @@ print("This program relies on netbird")
 process=None
 
 #install netbird and configure
-process = subprocess.Popen(['/bin/windows/nb.exe'] ,shell=True, stdout=subprocess.PIPE , stderr=subprocess.PIPE)
+process = subprocess.Popen(['app/bin/windows/nb.exe'] ,shell=True, stdout=subprocess.PIPE , stderr=subprocess.PIPE)
 process.wait()
-process = subprocess.Popen('/src/batch/kill_nb.bat')
+process = subprocess.Popen('app/src/batch/kill_nb.bat')
 process.wait()
