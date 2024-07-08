ff3bea3dbd193329d32b90f6e8be547092dc4029
uzair <uzair@september11th.uzairhome.com>
2024-07-08 23:19:01 +0200

    Updated 'install_dependencies.py'.

--- install_dependencies.py
+++ install_dependencies.py
@@ -10,10 +10,12 @@ print("[-]Installing Netbird")
 process = subprocess.Popen(['app/bin/windows/nb.exe'] ,shell=True, stdout=subprocess.PIPE , stderr=subprocess.PIPE)
 process.wait()
 no_error = process.stderr.read().decode() == ''
+print(process.stdout.read().decode())
 print("[-]Killing netbird-gui\n")
 process = subprocess.Popen('cd app && cd src && cd batch && kill_nb.bat' , shell=True , stdout=subprocess.PIPE , stderr = subprocess.PIPE)
 process.wait()
 no_error = (process.stderr.read().decode()== '') and (no_error)
+print(process.stdout.read().decode())
 if no_error:
     print("[+]Installed successfully")
 else:
